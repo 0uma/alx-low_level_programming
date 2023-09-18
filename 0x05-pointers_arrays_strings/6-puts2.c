33781d23a9423ddf0a1a84c8b4508343e5cd9fb2
Oumaima <Cherifoumaima7119@gmail.com>
2023-09-18 13:54:41 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,11 @@
 void puts2(char *str)
 {
 	int i;
-	int count = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
-		count++;
-	for (i = 0; i < count - 1; i += 2)
-		_putchar(str[i]);
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
