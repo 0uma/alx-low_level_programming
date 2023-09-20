043bdbc88d9099d47fd47ecf30900895f81d3796
Oumaima <Cherifoumaima7119@gmail.com>
2023-09-20 13:32:55 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - a function that copies strings
+ * @dest: 1st parameter
+ * @src: 2nd parameter
+ * @n: 3rd parameter
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
